a7c5c9e49fc0a0d35f21d643af3c61af5fa37c54
Nick Moore <nick@zoic.org>
2014-06-12 23:17:10 +1000

    example cleanup

--- python/examples/django/example_app/views.py
+++ python/examples/django/example_app/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from tranquil.contexts import BaseContext
+from tranquil.django.contexts import DjangoModelContext
 
-# Create your views here.
+from example_app.models import *
+
+
+class SchoolContext(DjangoModelContext):
+    model = School
+
+
+class SubjectContext(DjangoModelContext):
+    model = Subject
+
+
+class CourseContext(DjangoModelContext):
+    model = Course
+
+
+class StudentContext(DjangoModelContext):
+    model = Student
+
+
+class RootContext(BaseContext):
+
+    def action_school(self):
+        return SchoolContext(self.request)
+
+    def action_subject(self):
+        return SubjectContext()
+
+    def action_course(self):
+        return CourseContext()
+
+    def action_student(self):
+        return StudentContext()
